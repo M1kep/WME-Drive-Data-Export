4f96b52095bc7eedf6876ed6465799d7b5883d21
Michael Poutre <M1kep.my.mail@gmail.com>
2017-04-02 16:33:32 -0700

    Condensed HTML element creation, updated included URLS, and changed bootstrapping thanks to suggestions from @mapomatic

--- DriveData.js
+++ DriveData.js
@@ -1,14 +1,11 @@
-/* global Waze */
+/* global Waze, W */
 
 // ==UserScript==
 // @name WME DrivesData
 // @namespace http://tampermonkey.net/
-// @version 0.1
+// @version 0.1.6a
 // @description Export Waze Drive Data
-// @include https://*.waze.com/editor/*
-// @include https://*.waze.com/*/editor/*
-// @exclude https://*.waze.com/user/editor/*
-// @exclude https://*.waze.com/*/user/editor/*
+// @include /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/.*$/
 // @grant none
 // ==/UserScript==
 
@@ -20,8 +17,15 @@
     var wDDIsLoaded = false;
     var wDDIsCalculated = false;
     
-    setTimeout(initialize, 700);
-    Waze.loginManager.events.register("login", null, initialize);
+    function bootStrap() {
+        if(W && W.loginManager && W.loginManager.isLoggedIn()) {
+            initialize();
+        } else {
+            setTimeout(bootStrap, 500);
+        }
+    }
+    
+    bootStrap();
     
     function initialize()
     {
@@ -30,59 +34,83 @@
             wDDDrivesDataArr = new Array(wDDTotalDrives);
         });
         
-        var wDDDiv = document.createElement('div');
-        wDDDiv.style.paddingTop = '6px';
-        $('#sidepanel-drives').append(wDDDiv);
-        
-        var wDDTitle = document.createElement('h3');
-        wDDTitle.innerHTML = 'Drive Data Export';
-        wDDDiv.appendChild(wDDTitle);
-        
-        var wDDDesc = document.createElement('p');
-        wDDDesc.innerHTML = 'Click the export button below to export all drive data.';
-        wDDDiv.appendChild(wDDDesc);
-        
-        var wDDForm = document.createElement('form');
-        wDDDiv.appendChild(wDDForm);
-        
-        var wDDSteps = document.createElement('ol');
-        wDDForm.appendChild(wDDSteps);
-        
-        //Create List Item for wDDSteps Ordered List
-        var wDDStepOneLi = document.createElement('li');
-        //Create the button to be added to the li
-        var wDDStepOneButton = document.createElement('input');
-        wDDStepOneButton.type = 'button';
-        wDDStepOneButton.value = 'Load Data';
-        wDDStepOneButton.onclick = function(){
-            wDDIsLoaded = false;
-            wDDLoadData(0);
-        };    
-        //Set the button to the li
-        wDDStepOneLi.appendChild(wDDStepOneButton);
-        //Add the li to the ol
-        wDDSteps.appendChild(wDDStepOneLi);
-        
-        var wDDStepTwoLi = document.createElement('li');
-        var wDDStepTwoButton = document.createElement('input');
-        wDDStepTwoButton.type = 'button';
-        wDDStepTwoButton.value = 'Convert Data';
-        wDDStepTwoButton.onclick = function(){
-            wDDIsCalculated = false;
-            wDDCalculateData();
-        };
-        wDDStepTwoLi.appendChild(wDDStepTwoButton);
-        wDDSteps.appendChild(wDDStepTwoLi);
-        
-        
-        var wDDLoadDataBtn = document.createElement('input');
+        $('<div>').css({paddingTop:'6px'}).append(
+            $('<h3>').text('Drive Data Export'),
+            $('<p>').text('Please click the buttons in order to export data.'),
+            $('<form>').append(
+                $('<ol>').append(
+                    $('<li>').append(
+                        $('<input>', {type:'button',value:'Load Data'}).click(function(){
+                            wDDIsLoaded = false;
+                            wDDLoadData(0);
+                        })
+                    ),
+                    $('<li>').append(
+                        $('<input>', {type:'button',value:'Convert Data'}).click(function(){
+                            wDDIsCalculated = false;
+                            wDDCalculateData();
+                        })
+                    ),
+                    $('<li>').append(
+                        $('<input>', {type:'button',value:'Export Data'}).click(wDDExportDrives())
+                    )
+                )
+            )
+        ).appendTo('#sidepanel-drives');
 
-        
-        var wDDExportBtn = document.createElement('input');
-        wDDExportBtn.type = 'button';
-        wDDExportBtn.value = 'Export';
-        wDDExportBtn.onclick = wDDExportDrives;
-        wDDForm.appendChild(wDDExportBtn);
+//        var wDDDiv = document.createElement('div');
+//        wDDDiv.style.paddingTop = '6px';
+//        $('#sidepanel-drives').append(wDDDiv);
+//        
+//        var wDDTitle = document.createElement('h3');
+//        wDDTitle.innerHTML = 'Drive Data Export';
+//        wDDDiv.appendChild(wDDTitle);
+//        
+//        var wDDDesc = document.createElement('p');
+//        wDDDesc.innerHTML = 'Click the export button below to export all drive data.';
+//        wDDDiv.appendChild(wDDDesc);
+//        
+//        var wDDForm = document.createElement('form');
+//        wDDDiv.appendChild(wDDForm);
+//        
+//        var wDDSteps = document.createElement('ol');
+//        wDDForm.appendChild(wDDSteps);
+//        
+//        //Create List Item for wDDSteps Ordered List
+//        var wDDStepOneLi = document.createElement('li');
+//        //Create the button to be added to the li
+//        var wDDStepOneButton = document.createElement('input');
+//        wDDStepOneButton.type = 'button';
+//        wDDStepOneButton.value = 'Load Data';
+//        wDDStepOneButton.onclick = function(){
+//            wDDIsLoaded = false;
+//            wDDLoadData(0);
+//        };    
+//        //Set the button to the li
+//        wDDStepOneLi.appendChild(wDDStepOneButton);
+//        //Add the li to the ol
+//        wDDSteps.appendChild(wDDStepOneLi);
+//        
+//        var wDDStepTwoLi = document.createElement('li');
+//        var wDDStepTwoButton = document.createElement('input');
+//        wDDStepTwoButton.type = 'button';
+//        wDDStepTwoButton.value = 'Convert Data';
+//        wDDStepTwoButton.onclick = function(){
+//            wDDIsCalculated = false;
+//            wDDCalculateData();
+//        };
+//        wDDStepTwoLi.appendChild(wDDStepTwoButton);
+//        wDDSteps.appendChild(wDDStepTwoLi);
+//        
+//        
+//        var wDDLoadDataBtn = document.createElement('input');
+//
+//        
+//        var wDDExportBtn = document.createElement('input');
+//        wDDExportBtn.type = 'button';
+//        wDDExportBtn.value = 'Export';
+//        wDDExportBtn.onclick = wDDExportDrives;
+//        wDDForm.appendChild(wDDExportBtn);
     }
     
     function wDDLoadData(offset)
